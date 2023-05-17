098b3e092d7b7b2064e1950ab947167618f70475
gisa chris <gisachris24@gmail.com>
2023-05-17 06:25:24 -0200

    add edgecase to task4

--- test/task4.test.js
+++ test/task4.test.js
@@ -7,11 +7,17 @@ describe('capitalisation unit testing', () => {
     //act
     const NewString = capitalString('text');
     const firstLetter = NewString.charAt(0);
+
+    if(typeof firstLetter === 'number'){
+      throw new Error('capitalisation can not work on numbers');
+    }
+
     if(!firstLetter == firstLetter.toUpperCase()){
       throw new Error('capitalisation did not work');
     }
 
     //assert
+    expect(typeof(firstLetter)).toBe('string');
     expect(NewString).toMatch(/[A-Z]/);
   })
 })
